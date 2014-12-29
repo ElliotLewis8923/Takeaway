6a2dd1602e9314311afefc10bc20396fb4f2370c
ElliotLewis8923 <elewis8923@gmail.com>
2014-12-29 16:38:28 +0000

    wrote 3rd test

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,17 +4,21 @@ describe Takeaway do
 
 	before(:each) do
 		menu = double('Menu')
-		takeaway = Takeaway.new(menu)
+		@takeaway = Takeaway.new(menu)
 	end
 
-
 	it 'has a menu' do
-		expect(takeaway.menu).not_to eq nil
+		expect(@takeaway.menu).not_to eq nil
 	end
 
 	it 'can place an order' do
 		order = double('Order')
-		expect(takeaway.place_order(order)).to eq 'You have successfully placed an order.'
+		expect(@takeaway.place_order(order)).to eq 'You have successfully placed an order.'
 	end
-	
+
+	it 'raises an exception for an incorrectly placed order' do
+		incorrect_order = double('Order')
+		expect { (@takeaway.place_order(incorrect_order)) }.to raise_exception
+	end
+
 end
