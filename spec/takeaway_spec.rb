e851114e71d7fbe6204ed2c40ea0b6b4ac78cca7
ElliotLewis8923 <elewis8923@gmail.com>
2014-12-29 15:58:57 +0000

    passed 2nd test

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,16 +2,18 @@ require 'takeaway'
 
 describe Takeaway do 
 
-	it 'has a menu' do
+	before(:each) do
 		menu = double('Menu')
 		takeaway = Takeaway.new(menu)
+	end
+
+
+	it 'has a menu' do
 		expect(takeaway.menu).not_to eq nil
 	end
 
 	it 'can place an order' do
-		menu = double('Menu')
 		order = double('Order')
-		takeaway = Takeaway.new(menu)
 		expect(takeaway.place_order(order)).to eq 'You have successfully placed an order.'
 	end
 	
