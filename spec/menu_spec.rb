a429bf53bd31e942d6dfee7c97691ecd513224b1
ElliotLewis8923 <elewis8923@gmail.com>
2014-12-30 12:32:51 +0000

    refactored menu spec

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,12 +2,17 @@ require 'menu'
 
 describe Menu do 
 
-	let(:dish1) { double :dish }
-	let(:dish2) { double :dish }	
-	let(:dish3) { double :dish }
+	let(:dish1) { double :dish 					 }
+	let(:dish2) { double :dish 					 }	
+	let(:dish3) { double :dish 					 }
+	let(:menu)  { Menu.new(dish1, dish2) }
 
 	it 'is initialised with an optional quantity of dishes' do
-		menu = Menu.new(dish1, dish2, dish3)
+		expect(menu.dishes).to eq [dish1, dish2]
+	end
+
+	it 'can append dishes' do
+		menu.dishes << dish3
 		expect(menu.dishes).to eq [dish1, dish2, dish3]
 	end
 
