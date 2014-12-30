16c2175a9eef4b87f22e6aa1029cf91579d593c0
ElliotLewis8923 <elewis8923@gmail.com>
2014-12-30 12:26:39 +0000

    rewrote and passed first menu test

---
+++ lib/menu.rb
@@ -0,0 +1,9 @@
+class Menu
+
+	attr_accessor :dishes
+
+	def initialize(*dishes)
+		@dishes = dishes
+	end
+
+end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -7,8 +7,8 @@ describe Menu do
 	let(:dish3) { double :dish }
 
 	it 'is initialised with an optional quantity of dishes' do
-		menu = Menu.new([dish1, dish2, dish3])
-		expect(menu.dishes).not_to eq nil
+		menu = Menu.new(dish1, dish2, dish3)
+		expect(menu.dishes).to eq [dish1, dish2, dish3]
 	end
 
 end
