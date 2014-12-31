401ff716c0726acf6f2f2aa079d917a0720ac0ab
ElliotLewis8923 <elewis8923@gmail.com>
2014-12-31 14:30:28 +0000

    passed 5th order test; total cost is queried against the dishes' accumulated price

--- lib/order.rb
+++ lib/order.rb
@@ -11,4 +11,10 @@ class Order
 		@dishes[dish] ? @dishes[dish] += 1 : @dishes[dish] = 1
 	end
 
+	def correct?
+		memo = 0;
+		@dishes.each_pair { |k, v| memo += k.price * v }
+		memo == @total
+	end
+
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,11 +2,11 @@ require 'order'
 
 describe Order do
 
-	let(:order)  { Order.new(person)             			   	 }
+	let(:order)  { Order.new(person)             			   	     }
 	let(:person) { double :person                			  			 }
-	let(:dish1)  { double :dish                  			  			 }
-	let(:dish2)  { double :dish                  			  			 }
-	let(:dish3)  { double :dish                 			   			 }
+	let(:dish1)  { double :dish, name: 'Pizza', price: 3       }
+	let(:dish2)  { double :dish, name: 'Goulash', price: 5     }
+	let(:dish3)  { double :dish, name: 'Seabass', price: 4     }
 	let(:menu)   { double :menu, dishes: [dish1, dish2, dish3] }
 
 	it 'is initialised with a person' do
@@ -19,14 +19,21 @@ describe Order do
 	end
 
 	it 'records the quantity of each dish' do
-		order.add_dish(menu.dishes[0])
-		order.add_dish(menu.dishes[0])
+		2.times { order.add_dish(menu.dishes[0]) }
 		expect(order.dishes).to eq dish1 => 2
 	end
 
 	it 'has a user submitted total' do
 		order.total = 12
 		expect(order.total).to eq 12
+	end
+
+	it 'can query the user submitted total against its dishes accumulated price' do
+		menu.dishes.each { |e| order.add_dish(e) }
+		order.total = 12
+		expect(order.correct?).to eq true
+		order.total = 10
+		expect(order.correct?).to eq false
 	end
 
 end
